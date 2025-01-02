c41dcd2f0ab20557d77a78b2c0e02154da2f859e
ronjonson <neillaurenceortaliz@gmail.com>
2025-01-03 00:28:28 +0800

    init commit

---
+++ scripts/constants.py
@@ -0,0 +1,24 @@
+# Dropdowns
+region_dropdown = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[2]/nav-filter/div/span/div/div/span/span'
+province_dropdown = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[3]/nav-filter/div/span/div/div/span/span'
+city_dropdown = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[4]/nav-filter/div/span/div/div/span/span'
+brgy_dropdown = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[5]/nav-filter/div/span/div/div/span/span'
+precincts_dropdown = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[6]/nav-filter/div/span/div/div/span/span'
+
+
+region_list = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[2]/nav-filter/div/span/div/div/div[2]/ul/li[12]'
+province_list = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[3]/nav-filter/div/span/div/div/div[2]/ul/li[6]'
+city_list = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[4]/nav-filter/div/span/div/div/div[2]/ul/li[5]'
+brgy_list = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[5]/nav-filter/div/span/div/div/div[2]/ul'
+precincts_list = '//*[@id="container"]/ui-view/div/div/div[1]/nav/div/ul/li/div[5]/div[6]/nav-filter/div/span/div/div/div[2]/ul'
+
+# DATA
+precinct_metadata = '//*[@id="container"]/ui-view/div/div/div[2]/div[2]/div[2]/results-viewer/div[2]/div[1]/div[2]'
+president_table = '''//*[@id="'resultDiv.'+5587"]/div[1]'''
+vice_president_table = '''//*[@id="'resultDiv.'+5588"]/div[1]'''
+senator_table = '''//*[@id="'resultDiv.'+5589"]/div[1]'''
+partylist_table = '''//*[@id="'resultDiv.'+11172"]/div[1]'''
+rep_table = '''//*[@id="'resultDiv.'+6120"]/div[1]'''
+mayor_table = '''//*[@id="'resultDiv.'+7184"]/div[1]'''
+vice_mayor_table = '''//*[@id="'resultDiv.'+8818"]/div[1]'''
+sp_table = '''//*[@id="'resultDiv.'+10465"]/div[1]'''
---
+++ scripts/utils.py
@@ -0,0 +1,86 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+
+import pandas as pd
+
+from constants import *
+
+from dataclasses import dataclass, field
+
+@dataclass
+class Location:
+    country: str = field(default=None)
+    region: str = field(default=None)
+    province: str = field(default=None)
+    city: str = field(default=None)
+    barangay: str = field(default=None)
+    precinct: str = field(default=None)
+
+    def set_country(self, value: str=None):
+        self.country = value
+        # Reset lower hierarchy
+        self.set_region()
+
+    def set_region(self, value: str=None):
+        self.region = value
+        # Reset lower hierarchy
+        self.set_province()
+
+    def set_province(self, value: str=None):
+        self.province = value
+        # Reset lower hierarchy
+        self.set_city()
+
+    def set_city(self, value: str=None):
+        self.city = value
+        # Reset lower hierarchy
+        self.set_barangay()
+
+    def set_barangay(self, value: str=None):
+        self.barangay = value
+        # Reset lower hierarchy
+        self.set_precinct()
+
+    def set_precinct(self, value: str=None):
+        self.precinct = value
+
+
+    def __setattr__(self, name, value):
+        if name == "country":
+            self.set_country(value)
+        elif name == "region":
+            self.set_region(value)
+        elif name == "province":
+            self.set_province(value)
+        elif name == "city":
+            self.set_city(value)
+        elif name == "barangay":
+            self.set_barangay(value)
+        elif name == "precinct":
+            self.set_precinct(value)
+        else:
+            super().__setattr__(name, value)
+
+    def save_current_location(self):
+        pass
+
+
+def click_option(driver:webdriver, dropdown_xpath: str, list_xpath:str):
+    """Selects the given dropdown and area (Region, Province, City, Barangay, Precinct)"""
+    driver.find_element(by=By.XPATH, value=dropdown_xpath).click()
+    time.sleep(0.5)
+    driver.find_element(by=By.XPATH, value=list_xpath).click()
+    time.sleep(0.5)
+
+def get_dropdown_values(driver:webdriver, x_path:str):
+    dropdown_list = driver.find_element(by=By.XPATH, value=x_path)
+    values = dropdown_list.find_elements(by=By.TAG_NAME, value='li')
+
+    return values
+
+def clear_textbox(driver:webdriver, loc_arrow_xpath:str, textbox:str):
+    driver.find_element(By.XPATH,loc_arrow_xpath).click()
+    time.sleep(0.125)
+    driver.find_element(By.XPATH,textbox).clear()
